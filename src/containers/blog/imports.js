f5de4974f2fd74799d9e7bdbc12426dda046a6d6
Omkar Bharitkar <75433632+omkar2711@users.noreply.github.com>
2023-07-18 14:20:11 +0530

    Update imports.js

--- src/containers/blog/imports.js
+++ src/containers/blog/imports.js
@@ -1,4 +1,4 @@
-import blog1 from '../../assets/blog1.png';
+import blog1 from '../../assets/blog1.jpg';
 import blog2 from '../../assets/blog2.png';
 import blog3 from '../../assets/blog3.png';
 import blog4 from '../../assets/blog4.png';
